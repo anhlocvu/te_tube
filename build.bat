cca0acabd0a5ab96792494d6042f64f028f1ace9
LC_Boy <124279200+anhlocvu@users.noreply.github.com>
2026-03-16 12:59:32 +0700

    update build to version 1.3

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 title "start building"
-uv run -m nuitka --standalone --windows-console-mode=disable --lto=yes --remove-output --include-package=pyaudio --include-package=speech_recognition --include-package-data=speech_recognition --company-name="technology entertainment" --product-name="te_tube" --file-version="1.2" --product-version="1.2" --file-description="download, search, and listen to multimedia on YouTube" -o "te_tube.exe" main.py
+uv run -m nuitka --standalone --windows-console-mode=disable --lto=yes --remove-output --include-package=pyaudio --include-package=speech_recognition --include-package-data=speech_recognition --company-name="technology entertainment" --product-name="te_tube" --file-version="1.3" --product-version="1.3" --file-description="download, search, and listen to multimedia on YouTube" -o "te_tube.exe" main.py
 xcopy "lib" "main.dist/lib" /E /I /Y
 xcopy "updater" "main.dist" /E /I /Y
 xcopy "sounds" "main.dist/sounds" /E /I /Y
